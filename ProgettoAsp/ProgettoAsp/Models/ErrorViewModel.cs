48df876012f9bbb63e93e485f26b8ea5e4e3c72b
jeffrey ansah <j.ansah@ymail.com>
2019-09-02 14:10:18 +0200

    New file, Nuovoesercizio

---
+++ ProgettoAsp/ProgettoAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProgettoAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
